8a1cee725c218e150b859e84e6d15b55ad47b84f
ScoBoFoSho <ferrecisco@icloud.com>
2022-08-18 13:49:22 -0400

    classes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,24 @@
 const Employee = require("../lib/Employee.js");
+const employee = new Employee("Francisco", "262626", "ferrecisco@icloud.com");
 
-test("makes an employee");
+test("test to see if we can get constructor values from employee in lib", () => {
+  expect(employee.name).toBe("Francisco");
+  expect(employee.id).toBe("262626");
+  expect(employee.email).toBe("ferrecisco@icloud.com");
+});
+
+test("test to see if we can get name from getName() method", () => {
+  expect(employee.getName()).toBe("Francisco");
+});
+
+test("test to see if we can get ID from getId() method", () => {
+  expect(employee.getId()).toBe("262626");
+});
+
+test("test to see if we can get email from getEmail() method", () => {
+  expect(employee.getEmail()).toBe("ferrecisco@icloud.com");
+});
+
+test("test to see if we can get role from getRole() method", () => {
+  expect(employee.getRole()).toBe("Employee");
+});
