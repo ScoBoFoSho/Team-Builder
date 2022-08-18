fe198d296def100976622f524d0215fd0bab30e6
ScoBoFoSho <ferrecisco@icloud.com>
2022-08-18 19:38:04 -0400

    update inquirer/manager prompt questions

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 const fs = require("fs");
-const { default: inquirer } = require("inquirer");
+const path = require("path");
+const teamMembers = [];
 
-inquirer.prompt();
+const promptManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name? (REQUIRED)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your worker ID? (REQUIRED)",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter your ID!");
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your work email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office phone number?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(manager);
+    });
+};
+
+promptManager();
